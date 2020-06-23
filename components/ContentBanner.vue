c3a31ce11b790a081042bcdba2671af74fb3c903
njm222 <nathan.john.menezes@gmail.com>
2020-06-23 17:14:00 +0530

    added content banner

---
+++ components/ContentBanner.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="banner"
+       v-bind:class="{ right: index % 2 === 1 }"
+  >
+    <v-container class="banner-container"
+                 v-bind:class="{ right: index % 2 === 1 }"
+    >
+      <div class="banner-text">
+        <h1 class="banner-title" v-html="title"></h1>
+        <p class="banner-content" v-html="text"></p>
+      </div>
+      <div class="banner-image">
+        image will go here
+        <v-img></v-img>
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "Banner",
+      props: {
+        index: Number
+      },
+      data () {
+        return {
+          title: 'Make your Target Audience<br>Follow you',
+          text: 'Through thorough market analysis we identify who your customers are while showing you how to understand, approach and engage them <br><br> Identifying potential customers, targeting them and converting them to active leads.',
+        }
+      }
+    }
+</script>
+
+<style scoped>
+.banner {
+  width: 100%;
+}
+.banner.right {
+  background: #FFE500;
+}
+.banner-container {
+  padding: 5em 0;
+  display: flex;
+  justify-content: space-between;
+}
+.banner-container.right {
+  flex-direction: row-reverse;
+}
+.banner-text {
+  width: 45%;
+}
+.banner-image {
+  width: 45%;
+}
+</style>
--- pages/landing.vue
+++ pages/landing.vue
@@ -5,15 +5,20 @@
     align-center
   >
     <TopBanner />
+    <Banner :index="0" />
+    <Banner :index="1" />
+    <Banner :index="2" />
   </v-layout>
 </template>
 
 <script>
+import Banner from '~/components/ContentBanner'
 import Logo from '~/components/Logo.vue'
 import TopBanner from '~/components/TopBanner'
 
 export default {
   components: {
+    Banner,
     Logo,
     TopBanner
   }
