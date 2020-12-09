3153ecd1bf8b0871347a4c75c4f56f4857940ed2
NM <nathan.john.menezes@gmail.com>
2020-12-09 21:13:02 +0530

    cube animation improvements

--- components/CubeThree.vue
+++ components/CubeThree.vue
@@ -17,8 +17,9 @@
           scene: null,
           camera: null,
           exploding: false,
+          y: 1,
           particleSize: 1,
-          particleColour: 0x2828FF,
+          particleColour: 0xA5402D,
           particleSpeed: 10,
           dirs: [],
           limit: 300,
@@ -55,8 +56,8 @@
 
 
           /** Particle setup */
-          const geometry = new THREE.BoxGeometry(30, 30, 30, 15, 15, 15)
-          const particleGeo = new THREE.BoxBufferGeometry(2, 2, 2)
+          const geometry = new THREE.BoxGeometry(30, 30, 30, 5, 5, 5)
+          const particleGeo = new THREE.BoxBufferGeometry(6, 6, 6)
           const particleMat = new THREE.MeshLambertMaterial({color: this.particleColour, precision: 'highp' })
           const particleCube = new THREE.Mesh(particleGeo, particleMat)
           this.cube = new THREE.Group()
@@ -72,9 +73,9 @@
 
             /** direction vector of each particle */
             this.dirs[i] = ({
-              x:(Math.random() * this.particleSpeed)-(this.particleSpeed/2),
-              y:(Math.random() * this.particleSpeed)-(this.particleSpeed/2),
-              z:(Math.random() * this.particleSpeed)-(this.particleSpeed/2)
+              x:(Math.random() * this.particleSpeed)-(this.particleSpeed/3),
+              y:(Math.random() * this.particleSpeed)-(this.particleSpeed/3),
+              z:(Math.random() * this.particleSpeed)-(this.particleSpeed/3)
             });
           }
           this.cube.position.set(window.innerWidth * 3 / 100, 5, -5)
@@ -95,7 +96,7 @@
             this.cube.rotation.x += 0.01
             this.cube.rotation.y += 0.01
           } else {
-            this.explode(this.cube)
+            this.explode(this.y)
           }
           this.stats.end()
           requestAnimationFrame(this.animate)
@@ -103,28 +104,34 @@
           this.renderer.render(this.scene, this.camera)
         },
         initExplode () {
+          if (this.interval === 0) {
+            this.y = Math.random() * 2
+          }
           this.exploding = !this.exploding
         },
-        explode () {
+        explode (y) {
           let particleCount = this.cubeParticles.length;
           while(particleCount-- > 0) {
             const particle =  this.cubeParticles[particleCount]
             if (this.interval >= 100) {
-              particle.position.x -= this.dirs[particleCount].x
-              particle.position.y -= this.dirs[particleCount].y
-              particle.position.z -= this.dirs[particleCount].z
-              particle.rotation.x -= this.dirs[particleCount].x / 10
-              particle.rotation.z -= this.dirs[particleCount].z / 10
-            } else if ( this.interval < 80) {
-              particle.position.x += this.dirs[particleCount].x
-              particle.position.y += this.dirs[particleCount].y
-              particle.position.z += this.dirs[particleCount].z
-              particle.rotation.x += this.dirs[particleCount].x / 10
-              particle.rotation.z += this.dirs[particleCount].z / 10
-              // add gravity
+              const multiplier = Math.sqrt(Math.pow(this.interval - 99, y))
+              particle.position.x -= this.dirs[particleCount].x / multiplier
+              particle.position.y -= this.dirs[particleCount].y / multiplier
+              particle.position.z -= this.dirs[particleCount].z / multiplier
+              particle.rotation.x -= this.dirs[particleCount].x / (10 * multiplier)
+              particle.rotation.z -= this.dirs[particleCount].z / (10 * multiplier)
+            } else if (this.interval < 80) {
+              const multiplier = Math.sqrt(Math.pow(this.interval + 1, y))
+              particle.position.x += this.dirs[particleCount].x / multiplier
+              particle.position.y += this.dirs[particleCount].y / multiplier
+              particle.position.z += this.dirs[particleCount].z / multiplier
+              particle.rotation.x += this.dirs[particleCount].x / (10 * multiplier)
+              particle.rotation.z += this.dirs[particleCount].z / (10 * multiplier)
             }
           }
+
           this.interval++
+
           if (this.interval >= 180) {
             // reset interval
             this.interval = 0
