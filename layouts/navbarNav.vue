be686a2a09f36e493a1d789957cde4b9b22cd756
NM <nathan.john.menezes@gmail.com>
2020-12-09 21:12:12 +0530

    navbar style

--- layouts/navbarNav.vue
+++ layouts/navbarNav.vue
@@ -1,13 +1,56 @@
 <template>
-  $END$
+  <v-container>
+    <v-row>
+      <v-col v-for="item in navItems" :key="item.label">
+        <a v-on:click="navigate(item.value)" :href="item.link" class="navbar-nav-item">
+          <v-btn
+            v-if="item.alternate"
+            rounded
+            raised
+            dark
+          >
+            {{item.label}}
+          </v-btn>
+          <v-btn
+            v-else
+            text
+          >
+            <v-icon>{{ item.icon }}</v-icon>
+            {{item.label}}
+          </v-btn>
+        </a>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
   export default {
-    name: "navbarNav"
+    name: 'navbarNav',
+    data() {
+      return {
+        navItems: [
+          {label: 'Our Services', value: '#services'},
+          {label: '+91 832 908 6623', link: 'tel:+918329086623', icon: 'mdi-whatsapp'},
+          {label: 'Contact Us', value: '#contact', alternate: true}
+        ]
+      }
+    },
+    methods: {
+      navigate(ref) {
+        if(!ref) return
+
+        document.querySelector(ref).scrollIntoView({
+          behavior: 'smooth'
+        });
+      }
+    }
   }
 </script>
 
 <style scoped>
-
+.navbar-nav-item {
+  color: #131313;
+  text-decoration: none;
+}
 </style>
