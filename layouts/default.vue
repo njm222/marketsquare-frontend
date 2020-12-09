3e18cf15220f7de398a96a1416c068761b0d9839
NM <nathan.john.menezes@gmail.com>
2020-12-09 21:12:44 +0530

    mobile style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,114 +1,96 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-app-bar
       color="#FFFF00"
       :clipped-left="clipped"
       fixed
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
       <v-toolbar-title v-text="title" />
       <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
+      <div
+      v-if="!isMobile"
       >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+        <navbar-nav></navbar-nav>
+      </div>
+      <v-app-bar-nav-icon
+        v-if="isMobile"
+        @click.stop="drawer = !drawer"
+      />
+      <v-navigation-drawer
+        v-model="drawer"
+        :mini-variant="miniVariant"
+        :right="right"
+        :clipped="clipped"
+        fixed
+        app
+      >
+        <v-list>
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            :to="item.to"
+            router
+            exact
+          >
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title" />
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
     </v-app-bar>
     <v-main>
       <nuxt />
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <Footer />
   </v-app>
 </template>
 
 <script>
-import Footer from '~/components/Footer'
+  import Footer from '~/components/Footer'
+  import NavbarNav from "~/layouts/navbarNav";
+
 export default {
-  components: { Footer },
+  components: {NavbarNav, Footer },
   data () {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
+      isMobile: false,
       items: [
         {
           icon: 'mdi-apps',
           title: 'Welcome',
           to: '/'
         },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Welcome',
-          to: '/landing'
-        }
       ],
       miniVariant: false,
       right: true,
-      rightDrawer: false,
       title: 'MarketSquare'
     }
-  }
+  },
+  beforeDestroy () {
+    if (typeof window === 'undefined') return
+
+    window.removeEventListener('resize', this.onResize, { passive: true })
+  },
+
+  mounted () {
+    this.onResize()
+
+    window.addEventListener('resize', this.onResize, { passive: true })
+  },
+
+  methods: {
+    onResize () {
+      this.isMobile = window.innerWidth < 768
+    },
+  },
 }
 </script>
 
