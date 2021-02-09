3ca57a193c01d050c29089434413e07d5bce8f87
njm222 <nathan.john.menezes@gmail.com>
2021-02-09 20:12:20 +0530

    refactor and style change

--- components/Services/ServicesDesktop.vue
+++ components/Services/ServicesDesktop.vue
@@ -98,16 +98,16 @@ export default {
   transition-property: opacity;
 }
 .service-card:hover .text {
-  opacity: 100%;
+  opacity: 1;
 }
 .service-card {
-  opacity: 50%;
+  opacity: 0.5;
   color: rgba(0, 0, 0, 0.87);
   transition-duration: 1s;
   transition-property: opacity, background-color, opacity, border, box-shadow;
 }
 .service-card:hover {
-  opacity: 100%;
+  opacity: 1;
   background-color: #FFE500;
   border: 2px #FFF solid;
   box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
